426fee2f4986fb6d6015f0bbd3550f8d442cfb7b
Simon Schneider <sim@raynigon.de>
2018-10-06 09:06:39 +0200

    Added Single Run

--- backup.sh
+++ backup.sh
@@ -40,6 +40,10 @@ bin/storeBackup.pl --print -f config.cfg
 log "INFO " "Started Backup"
 while [ true ]; do
     bin/storeBackup.pl -f config.cfg
+    if [ !$RUN_ENDLESS ]; then
+        log "INFO" "Cronjob is disabled, terminating. Good Bye"
+        break
+    fi
     current_epoch=$(date +%s.%N)
     target_epoch=$(date -d 'tomorrow 00:00' +%s)
     sleep_seconds=$(echo "$target_epoch - $current_epoch" | bc)
